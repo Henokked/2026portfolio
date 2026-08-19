3f1fee14e53baab58bb3648e3ddfb6e82a146e68
Henokked <henok0ed@gmail.com>
2026-08-20 00:32:57 +0300

    Update resume link

--- constants.tsx
+++ constants.tsx
@@ -181,7 +181,7 @@ export const EDUCATION = [
 export const SOCIAL_LINKS = [
   { name: 'LinkedIn', url: 'https://et.linkedin.com/in/henok-edmealem-21a976197' },
   { name: 'Behance', url: 'https://behance.net/henok_ed' },
-  { name: 'Resume', url: 'https://drive.google.com/file/d/1lhOdQ2xe3IAmDO5R3T-A_RX12ngslN76/view' },
+  { name: 'Resume', url: 'https://drive.google.com/file/d/1Y7XWEKwxujvQp7qAhf9fTFVJJbfqgFcE/view?usp=sharing' },
   { name: 'Phone', url: 'tel:+251920958682' },
   { name: 'Email', url: 'mailto:henok0ed@gmail.com' }
 ];
